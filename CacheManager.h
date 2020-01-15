cbd93ae2d9db5480449c382e6848d40999772164
cs-dimitri <turovetsd@icloud.com>
2020-01-15 13:09:39 +0200

    Cache is done for string

--- CacheManager.h
+++ CacheManager.h
@@ -1,12 +1,16 @@
 #ifndef _CacheManager_h_
 #define _CacheManager_h_
+#include <stdio.h>
 
 template <typename Problem, typename Solution>
 
 class CacheManager
 {
-private:
-    /* data */
 public:
+    virtual bool existSolution(const Problem& problem) = 0;
+
+    virtual size_t makeHash(const Problem& problem) = 0;
+
+    virtual void addSolutionToBase(const Solution& solution,const Problem& problem) = 0;
 };
 #endif
---
+++ FileCacheManager.cpp
@@ -0,0 +1,41 @@
+#include "FileCacheManager.h"
+#include "CacheManager.h"
+
+#include <string>
+#include <stdio.h>
+#include <stdexcept>
+#include <iostream>
+#include <unordered_map>
+
+using namespace std;
+
+template <typename Problem, typename Solution>
+
+size_t FileCacheManager<Problem, Solution>::makeHash(const Problem &problem)
+{
+    size_t hashedProblem = hash<Problem>{}(problem);
+    return hashedProblem;
+}
+
+template <typename Problem, typename Solution>
+
+void FileCacheManager<Problem, Solution>::addSolutionToBase(const Solution &solution,
+                                                            const Problem &problem)
+{
+    this->fileSolution.insert({makeHash(problem), solution});
+}
+
+template <typename Problem, typename Solution>
+bool FileCacheManager<Problem, Solution>::existSolution(const Problem &problem)
+{
+
+    try
+    {
+        this->fileSolution.at(makeHash(problem));
+        return true;
+    }
+    catch (const std::out_of_range &oor)
+    {
+        return false;
+    }
+}
