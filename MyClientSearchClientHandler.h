15a17525f755f4d7eaa0b866c1272d21ba40406a
cs-dimitri <turovetsd@icloud.com>
2020-01-20 01:41:25 +0200

    matrix was received  May start implementing a algo

---
+++ MyClientSearchClientHandler.h
@@ -0,0 +1,32 @@
+#ifndef _MyClientSearchClientHandler_h
+#define _MyClientSearchClientHandler_h
+#include "ClientHandler.h"
+#include <string>
+#include "Solver.h"
+#include "CacheManager.h"
+#include "Matrix.h"
+#include "State.h"
+#include <vector>
+#include "CellMatrix.h"
+
+using namespace std;
+
+class MyClientSearchClientHandler : public ClientHandler
+{
+private:
+
+  CacheManager<Matrix<CellMatrix>, vector<State<CellMatrix>>> *cache;
+  Solver<Matrix<CellMatrix>, vector<State<CellMatrix>>> *solver;
+
+public:
+  MyClientSearchClientHandler(CacheManager<Matrix<CellMatrix>, vector<State<CellMatrix>>> *cache_manager,
+      Solver<Matrix<CellMatrix>, vector<State<CellMatrix>>> *problem_solver)
+  {
+
+    this->cache = cache_manager;
+    this->solver = problem_solver;
+  }
+  void handleClient(int port) override;
+};
+
+#endif
